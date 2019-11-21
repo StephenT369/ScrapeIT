35987b5a06184760777e8be336c0b8ab855388fa
Stephen Thompson <smt3@hotmail.com>
2019-11-21 16:36:26 -0600

    updated draft

--- models/ArticleData.js
+++ models/ArticleData.js
@@ -1,38 +1,27 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-
 var ArticleDataSchema = new Schema({
-	headline: {
-		type: String,
-		required: true,
-	},
-	url: {
-		type: String,
-		required: true,
-	},
-	summary: {
-		type: String,
-		default: "No Summary"
-	},
-	saved: {
-		type: Boolean,
-		default: false
-	},
-	status: {
-		type: String,
-		default: "Saved Article"
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	note: {
-		type: Schema.Types.ObjectId,
-		ref: "Note"
-	}
+  headline: {
+    type: String,
+    required: true,
+    unique: { index: { unique: true } }
+  },
+  summary: {
+    type: String,
+    default: "No Summary"
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  }
 });
-
-ArticleDataSchema.index({title: "text"});
-
-var Article  = mongoose.model("Article", ArticleDataSchema);
+var Article = mongoose.model("Article", ArticleDataSchema);
 module.exports = Article;
--- models/NoteData.js
+++ models/NoteData.js
@@ -1,14 +1,15 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-
 var NoteDataSchema = new Schema({
-	title: {
-		type: String,
-	},
-	body: {
-		type: String,
-	}
+  _articleId: {
+    type: Schema.Types.ObjectId,
+    ref: "Article"
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  noteText: String
 });
-
 var Note = mongoose.model("Note", NoteDataSchema);
 module.exports = Note;
